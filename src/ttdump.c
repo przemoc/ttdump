272849e6c294605abd13a8920104a415fa28d513
Przemyslaw Pawelczyk <przemoc@gmail.com>
2014-10-08 00:05:33 +0200

    Support dumping from many interfaces and data copying.
    It's not an efficient implementation, but should mostly work.
    TUN/TAP interfaces are no longer switched to non-blocking mode.

--- src/ttdump.c
+++ src/ttdump.c
@@ -34,10 +34,19 @@
 
 #define MAX_FRAME_SIZE (9126 + 14)  /* jumbo + header w/o FCS */
 
+#define MAX_IF_COUNT 16
+
+#define _str(x) #x
+#define str(x) _str(x)
+
 
 const char ttdump_usage[] =
-	"Usage: %s DEVNAME {tun|tap}\n"
-	"Dump packets/frames reaching given TUN/TAP interface.\n"
+	"Usage: %s [-c] DEVNAME... {tun|tap}\n"
+	"\n"
+	"Dump packets/frames reaching given TUN/TAP interfaces "
+	"(up to " str(MAX_IF_COUNT) ").\n"
+	"If -c option is given, then data received on some interface is sent to "
+	"the next one (which is the same one if there is only one interface).\n"
 	;
 
 
@@ -48,77 +57,127 @@ int print_err(const char *fname, int ret)
 }
 
 
-int check_and_parse(int argc, char *argv[], char *devnam, int *mode)
+int print_err_prefix(const char *prefix, const char *fname, int ret)
 {
+	return fprintf(stderr, "%s %s() returned %d, errno = %d (%s).\n",
+	               prefix, fname, ret, errno, strerror(errno));
+}
+
+
+int check_and_parse(int argc, char *argv[], char *devnams,
+                    int *mode, int *copy)
+{
+	int pos = 1;
+
 	if (argc == 1) {
 		printf(ttdump_usage, argv[0]);
 		return -1;
 	}
 
-	if (argc != 3) {
+	if (!strcmp(argv[1], "-c")) {
+		pos++;
+		*copy = 1;
+	}
+
+	if (argc < pos + 2) {
 		fprintf(stderr, "You have to specify TUN/TAP device name and mode!\n");
 		return -2;
 	}
 
-	*mode =   !strcmp(argv[2], "tun") * TT_FLG_TUN
-	        | !strcmp(argv[2], "tap") * TT_FLG_TAP;
-
-	if ((unsigned)(strlen(argv[1]) - 1) >= (TT_DEVNAMSIZ - 1) || !*mode) {
-		fprintf(stderr, "Please give proper TUN/TAP device name and mode!\n");
+	if (argc - pos > MAX_IF_COUNT) {
+		fprintf(stderr, "You specified too many TUN/TAP devices!\n");
 		return -3;
 	}
 
-	strncpy(devnam, argv[1], TT_DEVNAMSIZ);
+	*mode =   !strcmp(argv[argc - 1], "tun") * TT_FLG_TUN
+	        | !strcmp(argv[argc - 1], "tap") * TT_FLG_TAP;
 
-	return 0;
+	if ((unsigned)(strlen(argv[1]) - 1) >= (TT_DEVNAMSIZ - 1) || !*mode) {
+		fprintf(stderr, "Please give proper TUN/TAP device name and mode!\n");
+		return -4;
+	}
+
+	for (int i = 0; i < argc - pos - 1; i++)
+		strncpy(devnams + i * TT_DEVNAMSIZ, argv[pos + i], TT_DEVNAMSIZ);
+
+	return argc - pos - 1;
 }
 
 
 int main(int argc, char *argv[])
 {
-	int fd, res, mode;
-	char devnam[TT_DEVNAMSIZ];
+	int fds, res, mode, copy = 0;
+	struct pollfd pfd[MAX_IF_COUNT] = { { 0 } };
+	char devnams[MAX_IF_COUNT][TT_DEVNAMSIZ] = { { 0 } };
 
-	res = check_and_parse(argc, argv, devnam, &mode);
-	if (res < 0)
-		return -1 - res;
+	fds = check_and_parse(argc, argv, devnams[0], &mode, &copy);
+	if (fds < 0)
+		return -1 - fds;
 
-	fd = tt_open(devnam,   mode
-	                     | TT_FLG_CLOEXEC | TT_FLG_NONBLOCK | TT_FLG_IFUP);
-	if (fd < 0) {
-		print_err("tt_open", fd);
-		return 3;
+	for (int i = 0; i < fds; i++) {
+		pfd[i].events = POLLIN;
+		printf("Opening %s...\n", devnams[i]);
+		pfd[i].fd = tt_open(devnams[i],   mode
+		                                | TT_FLG_CLOEXEC | TT_FLG_IFUP);
+		if (pfd[i].fd < 0) {
+			print_err("tt_open", pfd[i].fd);
+			return 4;
+		}
+		printf("%s opened.\n", devnams[i]);
 	}
 
-	struct pollfd pfd = { fd, POLLIN, 0 };
+	puts("");
+
 	char framebuf[MAX_FRAME_SIZE];
 	char *hexbuf = NULL;
 
-	for (;;) {
-		res = poll(&pfd, 1, -1);
+	for (int i = 0;;) {
+		res = poll(pfd, fds, -1);
 		if (res < 0) {
 			print_err("poll", res);
 			break;
 		}
 
-		res = read(fd, framebuf, sizeof(framebuf));
-		if (res == -1) {
-			if (errno == EINTR)
+		for (i = 0; i < fds; i++) {
+			if (!pfd[i].revents)
 				continue;
-			print_err("read", res);
-			break;
-		}
-		if (res == 0) {
-			fprintf(stderr, "read() returned 0.\n");
-			break;
+
+			res = read(pfd[i].fd, framebuf, sizeof(framebuf));
+			if (res == -1) {
+				if (errno == EINTR)
+					continue;
+				print_err_prefix(devnams[i], "read", res);
+				i = -1;
+				break;
+			}
+			if (res == 0) {
+				fprintf(stderr, "%s read() returned 0.\n", devnams[i]);
+				i = -1;
+				break;
+			}
+
+			dumphex(&hexbuf, framebuf, res,
+			        DHEX_SHOWADDR | DHEX_RELADDR | DHEX_SHOWCHAR, devnams[i]);
+			puts(hexbuf);
+
+			if (copy) {
+				res = write(pfd[(i + 1) % fds].fd, framebuf, res);
+				if (res == -1 && errno != EINTR) {
+					print_err_prefix(devnams[(i + 1) % fds], "write", res);
+					i = -1;
+					break;
+				}
+
+				printf("%s sent %d bytes.\n\n", devnams[(i + 1) % fds], res);
+			}
 		}
 
-		dumphex(&hexbuf, framebuf, res,
-		        DHEX_SHOWADDR | DHEX_RELADDR | DHEX_SHOWCHAR, NULL);
-		puts(hexbuf);
+		if (i < 0)
+			break;
 	}
 
-	close(fd);
+	while (--fds >= 0)
+		close(pfd[fds].fd);
 
 	return 0;
 }
